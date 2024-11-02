7b73b38675011560ef8c838b828039ee212d4fcd
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2024-11-02 18:29:47 +0530

    Fixed da dam thing

--- bank.py
+++ bank.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app= Flask(__name__)
 
 @app.route('/')
+def renderHome():
+    return render_template("index2.html")
+
+@app.route('/login')
 def renderLogin():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 if __name__=='__main__':
     app.run(debug=True) 
