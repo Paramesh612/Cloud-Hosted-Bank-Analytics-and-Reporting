ca455fc11bae66de37369f458d3b940c78ffee3f
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2024-11-08 15:44:55 +0530

    Test74

--- flask-banking-system-main/app/transactions/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-transaction = Blueprint('transaction', __name__)
-
-
-from . import views
--- flask-banking-system-main/celery_worker.py
+++
@@ -1,5 +0,0 @@
-from app import celery, create_app, tasks
-
-
-app = create_app()
-app.app_context().push()
