d4f04c367ece79fb8ded5195a9e37b76804a6b8d
Kishore-reigns <112459807+Kishore-reigns@users.noreply.github.com>
2024-11-08 15:43:35 +0530

    deposit

--- bank.py
+++ bank.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template, request, session
+from flask import *
+import mysql.connector
+
 
 app= Flask(__name__)
 
@@ -19,5 +22,90 @@ def renderLogin():
 def renderDashboard():
     return render_template("dashboard.html")
 
+
+
 if __name__=='__main__':
     app.run(debug=True) 
+
+
+@app.route("/dashboard")
+
+def dashboard():
+
+    user_data = session.get('user')
+
+    if user_data:
+
+        email = user_data['email']
+
+        conn = get_db_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+
+        user = cursor.fetchone()
+
+        cursor.close()
+
+        conn.close()
+
+        return render_template("dashboard.html", user=user)
+
+    else:
+
+        return redirect(url_for('login'))
+
+   
+
+@app.route("/deposit", methods=['post', 'get'])
+
+def deposit():
+
+    user_data = session.get('user')
+
+    if user_data:
+
+        if request.method == 'POST':
+
+            amount = float(request.form['deposit_amount'])
+
+            account_type = request.form['account_type']
+
+            conn = get_db_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT 1 FROM accounts WHERE user_id = %s", (user_data['user_id'],))
+
+            if not cursor.fetchone():
+
+                # Insert a new row
+
+                cursor.execute("INSERT INTO accounts (user_id, balance, account_type) VALUES (%s, %s, %s)", (user_data['user_id'], amount, account_type))
+
+                cursor.execute("INSERT INTO account_statements (user_id, transaction_type,transaction_amount, transaction_date) VALUES (%s, 'Credit', %s, %s)", (user_data['user_id'], amount, datetime.now()))
+
+            else:
+
+                # Update the existing row
+
+                cursor.execute("UPDATE accounts SET balance = balance + %s WHERE user_id = %s", (amount, user_data['user_id']))
+
+                cursor.execute("INSERT INTO account_statements (user_id, transaction_type,transaction_amount,  transaction_date) VALUES (%s, 'Credit', %s, %s)", (user_data['user_id'], amount, datetime.now()))
+
+            conn.commit()
+
+            cursor.close()
+
+            conn.close()
+
+            flash("Funds deposited successfully!")
+
+            return redirect(url_for('dashboard'))
+
+        return render_template("deposit.html")
+
+    else:
+
+        return redirect(url_for('login'))
